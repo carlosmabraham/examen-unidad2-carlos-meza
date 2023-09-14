2de54311ae9f0ae91f7116324c25271ab052c964
Carlos <carlosmabraham@gmail.com>
2023-09-14 01:01:04 -0700

    actualizacion 9/14/23

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ import { Base, Laberinto } from './Base.js'
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
-
 let dir = 0;
 let speed = 2;
 let walls = [];
@@ -18,44 +17,95 @@ image.src = "./img/ballena_2_24px_right.png";
 const whale = new Base(25, 30, 10, 10, "black");
 const food = new Base(350, 350, 40, 40, "red");
 
+//Prueba2
+walls.push(new Base(15, 30, 10, 710, "gray"));
+walls.push(new Base(715, 30, 10, 710, "gray"));
+walls.push(new Base(20, 730, 695, 10, "gray"));
+walls.push(new Base(50, 30, 665, 10, "gray"));9
 
-walls.push(new Base(15, 15, 5, 500, "gray"));
-walls.push(new Base(510, 15, 5, 450, "gray"));
-walls.push(new Base(20, 510, 450, 5, "gray"));
-walls.push(new Base(45, 15, 465, 5, "gray"));
+//Vertical
+//Linea No1
+walls.push(new Base(50, 40, 10, 70, "gray"));
+walls.push(new Base(50, 285, 10, 445, "gray"));
+//Linea No2
+walls.push(new Base(85, 40, 10, 35, "gray"));
+walls.push(new Base(85, 145, 10, 585, "gray"));
+//Linea No3
+walls.push(new Base(120, 75, 10, 60, "gray"));
+walls.push(new Base(120, 170, 10, 690, "gray"));
+//Linea No4
+walls.push(new Base(155, 40, 10, 690, "gray"));
+//Linea No5
+walls.push(new Base(190, 40, 10, 690, "gray"));
+//Linea No6
+walls.push(new Base(225, 40, 10, 690, "gray"));
+//Linea No7
+walls.push(new Base(260, 40, 10, 690, "gray"));
+//Linea No8
+walls.push(new Base(295, 40, 10, 690, "gray"));
+//Linea No9
+walls.push(new Base(330, 40, 10, 690, "gray"));
+//Linea No10
+walls.push(new Base(365, 40, 10, 690, "gray"));
+//Linea No11
+walls.push(new Base(400, 40, 10, 690, "gray"));
+//Linea No12
+walls.push(new Base(435, 40, 10, 690, "gray"));
+//Linea No13
+walls.push(new Base(470, 40, 10, 690, "gray"));
+//Linea No14
+walls.push(new Base(505, 40, 10, 690, "gray"));
+//Linea No15
+walls.push(new Base(540, 40, 10, 690, "gray"));
+//Linea No16
+walls.push(new Base(575, 40, 10, 690, "gray"));
+//Linea No17
+walls.push(new Base(610, 40, 10, 690, "gray"));
+//Linea No18
+walls.push(new Base(645, 40, 10, 690, "gray"));
+//Linea No19
+walls.push(new Base(680, 40, 10, 690, "gray"));
 
-//PRUEBA LABERINTO
-//VERTICAL
-walls.push(new Base(45, 21, 5, 45, "gray"));
-walls.push(new Base(70, 21, 5, 20, "gray"));//vertical
-walls.push(new Base(95, 40, 5, 50, "gray"));
-walls.push(new Base(155, 21, 5, 20, "gray"));
-walls.push(new Base(180, 41, 5, 20, "gray"));
-walls.push(new Base(205, 21, 5, 20, "gray"));
-walls.push(new Base(244, 41, 5, 20, "gray"));
-walls.push(new Base(283, 21, 5, 40, "gray"));
-walls.push(new Base(155, 66, 5, 24, "gray"));
-walls.push(new Base(268, 66, 5, 24, "gray"));
-walls.push(new Base(308, 41, 5, 40, "gray"));
-walls.push(new Base(230, 91, 5, 19, "gray"));
-walls.push(new Base(120, 90, 5, 50, "gray"));
-walls.push(new Base(180, 89, 5, 20, "gray"));
 
-
-
-//HORIZONTAL
-walls.push(new Base(50, 61, 45, 5, "gray"));
-walls.push(new Base(120, 36, 33, 5, "gray"));
-walls.push(new Base(230, 36, 33, 5, "gray"));
-walls.push(new Base(120, 61, 129, 5, "gray"));
-walls.push(new Base(268, 61, 20, 5, "gray"));
-walls.push(new Base(308, 36, 65, 5, "gray"));
-walls.push(new Base(100, 85, 54, 5, "gray"));
-walls.push(new Base(230, 85, 36, 5, "gray"));
-walls.push(new Base(293, 83, 20, 5, "gray"));
-walls.push(new Base(150, 110, 85, 5, "gray"));
-walls.push(new Base(180, 83, 25, 5, "gray"));
-
+//Horizontal
+//Linea No1
+walls.push(new Base(160, 65, 555, 10, "gray"));
+//Linea No2
+walls.push(new Base(55, 100, 660, 10, "gray"));
+//Linea No3
+walls.push(new Base(20, 135, 695, 10, "gray"));
+//Linea No4
+walls.push(new Base(20, 170, 695, 10, "gray"));
+//Linea No5
+walls.push(new Base(20, 205, 695, 10, "gray"));
+//Linea No6
+walls.push(new Base(20, 240, 695, 10, "gray"));
+//Linea No7
+walls.push(new Base(20, 275, 695, 10, "gray"));
+//Linea No8
+walls.push(new Base(20, 310, 695, 10, "gray"));
+//Linea No9
+walls.push(new Base(20, 345, 695, 10, "gray"));
+//Linea No10
+walls.push(new Base(20, 380, 695, 10, "gray"));
+//Linea No11
+walls.push(new Base(20, 415, 695, 10, "gray"));
+//Linea No12
+walls.push(new Base(20, 450, 695, 10, "gray"));
+//Linea No13
+walls.push(new Base(20, 485, 695, 10, "gray"));
+//Linea No14
+walls.push(new Base(20, 520, 695, 10, "gray"));
+//Linea No15
+walls.push(new Base(20, 555, 695, 10, "gray"));
+//Linea No16
+walls.push(new Base(20, 590, 695, 10, "gray"));
+//Linea No17
+walls.push(new Base(20, 625, 695, 10, "gray"));
+//Linea No18
+walls.push(new Base(20, 660, 695, 10, "gray"));
+//Linea No19
+walls.push(new Base(20, 695, 695, 10, "gray"));
 
 
 document.addEventListener("keydown", (e) =>{
@@ -163,6 +213,7 @@ const paint = () => {
         for (let i = walls.length - 1; i >= 0; i--) {
             walls[i].paint(ctx)
         }
+
     }else{
 
         ctx.fillStyle = "rgba(154,160,154,0.020)";
